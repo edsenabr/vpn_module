8c923187533c4856e39d77790a73d307f3134022
Eduardo Sena S. Rosa <edsena@google.com>
2024-08-26 12:34:54 -0300

    adding project id to nw resources

--- network.tf
+++ network.tf
@@ -1,9 +1,11 @@
 resource "google_compute_address" "vpn" {
+  project = var.project_id
   name = "vpn-server-address"
   region = var.region
 }
 
 resource "google_compute_firewall" "vpn" {
+  project = var.project_id
   name    = "vpn-server-ingress"
   network =  var.vpc.self_link
   target_tags = ["vpn"]
